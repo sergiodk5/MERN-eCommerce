0f1f134a444d1426cf2effd03c55d15dc70570e1
Asterios Patsikas <sergiodk5@gmail.com>
2020-12-09 13:25:55 +0200

    Front end React Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to MERN Shop</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
